5eba94e9d82463806e7e30f8b8414f54b1b582e0
Daneality <dgolmov@gmail.com>
2023-02-28 10:45:01 +0100

    final version

--- blog-frontend/src/index.tsx
+++ blog-frontend/src/index.tsx
@@ -3,14 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Auth0Provider } from './contexts/auth0_context';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Auth0Provider>
+	<BrowserRouter>
+	<App />
+	</BrowserRouter>
+  </Auth0Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
